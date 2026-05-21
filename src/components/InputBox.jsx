0b4a022b889ad2fa3bcfd2442e77e2f88d35b699
abdulrdeveloper <abdulrahman2040b@gmail.com>
2026-05-21 16:01:48 +0500

    feat: implement currency conversion functionality with InputBox component

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,22 +1,17 @@
-
 function InputBox({
     label,
     amount,
     onAmountChange,
     onCurrencyChange,
-    currencyOption = [],
+    currencyOptions = [],
     selectCurrency,
-    amountDisable = false,
-    currencyDisable = false,
     className = "",
+    amountDisabled = false,
 }) {
-   
-
     return (
         <div className={`bg-white p-3 rounded-lg text-sm flex `}>
             <div className="w-1/2">
-                
-                <label  className="text-black/40 mb-2 inline-block">
+                <label className="text-black/40 mb-2 inline-block">
                     {label}
                 </label>
 
@@ -24,25 +19,21 @@ function InputBox({
                     className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount"
-                    disabled={amountDisable}
+                    disabled={amountDisabled}
                     value={amount}
-                    onChange={(e) => onAmountChange && onAmountChange(e.target.value)}
+                    onChange={(e) =>  onAmountChange && onAmountChange(Number(e.target.value))}
                 />
             </div>
             <div className="w-1/2 flex flex-wrap justify-end text-right">
                 <p className="text-black/40 mb-2 w-full">Currency Type</p>
-                <select
+                <select 
                     className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
-                    disabled={currencyDisable}
-                    value={selectCurrency}                                                                             
+                    value={selectCurrency}
                     onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
                 >
-                    {currencyOption.map((currency) => (
-                        <option key={currency} value={currency}>
-                            {currency}
-                        </option>
+                    {currencyOptions.map((currency) => (
+                        <option key={currency} value={currency}>{currency}</option>
                     ))}
-
                 </select>
             </div>
         </div>
